ea6cb306f3886a78257d309f80b3bce0ae97ab99
Danil <danil.r.savchenko@gmail.com>
2025-07-08 19:17:32 +0300

    feat: Initialize project

---
+++ src/main/java/com/danilko/product_catalog_cache_system/ProductCatalogCacheSystemApplication.java
@@ -0,0 +1,13 @@
+package com.danilko.product_catalog_cache_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogCacheSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogCacheSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilko/product_catalog_cache_system/ProductCatalogCacheSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilko.product_catalog_cache_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogCacheSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
