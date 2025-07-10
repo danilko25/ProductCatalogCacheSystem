4408e45d60e6241bcfd4fa969bf3e8d0b834ec58
Danil <danil.r.savchenko@gmail.com>
2025-07-10 17:37:20 +0300

    feat(security): Configure BaseAuth with Spring Security

---
+++ src/main/java/com/danilko/product_catalog_cache_system/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.danilko.product_catalog_cache_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/api/v1/categories/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/categories/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/categories/**").authenticated()
+
+                        .requestMatchers(HttpMethod.POST, "/api/v1/products/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/products/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/products/**").authenticated()
+
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
