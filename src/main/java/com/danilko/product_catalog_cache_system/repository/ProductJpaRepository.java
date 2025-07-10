3e412080a5582352908573561053f2dbef5b56d5
Danil <danil.r.savchenko@gmail.com>
2025-07-10 11:41:10 +0300

    fix(jpa): Address N+1 query problem for Product pagination

--- src/main/java/com/danilko/product_catalog_cache_system/repository/ProductJpaRepository.java
+++ src/main/java/com/danilko/product_catalog_cache_system/repository/ProductJpaRepository.java
@@ -1,7 +1,10 @@
 package com.danilko.product_catalog_cache_system.repository;
 
 import com.danilko.product_catalog_cache_system.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +17,6 @@ public interface ProductJpaRepository extends JpaRepository<Product, Long> {
     @NonNull
     List<Product> findAllByCategoryId(@NonNull Long id);
 
+    @Query(value = "SELECT p FROM Product p JOIN FETCH p.category", countQuery = "SELECT count (p) FROM Product p")
+    Page<Product> findAllWithCategory(Pageable pageable);
 }
--- src/main/java/com/danilko/product_catalog_cache_system/service/impl/ProductServiceImpl.java
+++ src/main/java/com/danilko/product_catalog_cache_system/service/impl/ProductServiceImpl.java
@@ -32,7 +32,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Page<ProductReadDto> findAll(Pageable pageable) {
-        var products = productRepository.findAll(pageable);
+        var products = productRepository.findAllWithCategory(pageable);
         return products.map(productReadDtoMapper::mapFrom);
     }
 
